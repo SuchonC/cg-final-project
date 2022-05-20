276e7cf4bdad6830003633ea51f5c95354b7a9a3
SuchonC <suchon_foke@windowslive.com>
2022-05-20 14:30:03 +0700

    chore: remove unused mouse event registers

--- src/common/SimpleRenderer.js
+++ src/common/SimpleRenderer.js
@@ -59,14 +59,6 @@ export default class SimpleRenderer {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
 
-    // this.renderer.domElement.addEventListener(
-    //   "mousedown",
-    //   this.onMouseDown.bind(this)
-    // );
-    // this.renderer.domElement.addEventListener(
-    //   "mouseup",
-    //   this.onMouseUp.bind(this)
-    // );
     // composer
     this.composer = new EffectComposer(this.renderer);
 
@@ -83,25 +75,6 @@ export default class SimpleRenderer {
       "mousemove",
       this.onMouseMove.bind(this)
     );
-    // this.renderer.domElement.addEventListener(
-    //   "wheel",
-    //   this.onMouseWheel.bind(this)
-    // );
-    // this.renderer.domElement.addEventListener(
-    //   "touchstart",
-    //   this.touchStart.bind(this),
-    //   false
-    // );
-    // this.renderer.domElement.addEventListener(
-    //   "touchmove",
-    //   this.touchMove.bind(this),
-    //   false
-    // );
-    // this.renderer.domElement.addEventListener(
-    //   "touchend",
-    //   this.touchEnd.bind(this),
-    //   false
-    // );
 
     this.updateCamera();
     this.render();
